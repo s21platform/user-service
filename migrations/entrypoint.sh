7a773351f917c1010b601f19cf25f240cb79b624
Aleksey Popov <asepopov@avito.ru>
2024-08-01 22:13:58 +0300

    try new logic of build migrate script

--- migrations/entrypoint.sh
+++ migrations/entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-ls /app/db/migrations
-
 # Применим миграции
-goose -dir /app/db/migrations postgres "user=$USER_SERVICE_POSTGRES_USER password=$USER_SERVICE_POSTGRES_PASSWORD dbname=$USER_SERVICE_POSTGRES_DB host=$USER_SERVICE_POSTGRES_HOST port=$USER_SERVICE_POSTGRES_PORT sslmode=disable" up
+# ВАЖНО! Для локального запуска вызвать из корня проекта, предварительно подгрузив переменные окружения в терминал командой  `set -a; source <путь к .env файлу>; set +a`
+# Установка goose: go install github.com/pressly/goose/v3/cmd/goose@latest
+goose -dir ./migrations postgres "user=$USER_SERVICE_POSTGRES_USER password=$USER_SERVICE_POSTGRES_PASSWORD dbname=$USER_SERVICE_POSTGRES_DB host=$USER_SERVICE_POSTGRES_HOST port=$USER_SERVICE_POSTGRES_PORT sslmode=disable" up
