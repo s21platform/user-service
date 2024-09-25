21b80d23c18d8e06be4f80a6b56f1baeb3e3fd94
asepopov <asepopov@avito.ru>
2024-09-25 15:45:28 +0300

    add handler for get info

--- internal/model/user.go
+++ internal/model/user.go
@@ -1,16 +1,16 @@
 package model
 
 type UserInfo struct {
-	Nickname       string `db:"login"`
-	LastAvatarLink string `db:"last_avatar_link"`
-	Name           string `db:"name"`
-	Surname        string `db:"surname"`
-	Birthdate      string `db:"birthdate"`
-	Phone          string `db:"phone"`
-	Telegram       string `db:"telegram"`
-	Git            string `db:"git"`
-	CityId         string `db:"city_id"`
-	OSId           string `db:"os_id"`
-	WorkId         string `db:"work_id"`
-	UniversityId   string `db:"university_id"`
+	Nickname       string  `db:"login"`
+	LastAvatarLink string  `db:"last_avatar_link"`
+	Name           *string `db:"name"`
+	Surname        *string `db:"surname"`
+	Birthdate      *string `db:"birthdate"`
+	Phone          *string `db:"phone"`
+	Telegram       *string `db:"telegram"`
+	Git            *string `db:"git"`
+	CityId         *string `db:"city_id"`
+	OSId           *string `db:"os_id"`
+	WorkId         *string `db:"work_id"`
+	UniversityId   *string `db:"university_id"`
 }
