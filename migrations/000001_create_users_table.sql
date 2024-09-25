9054900cb1cbe3649de6a566150d287abeb4fd0a
asepopov <asepopov@avito.ru>
2024-09-25 11:09:35 +0300

    reformat migration

--- migrations/000001_create_users_table.sql
+++ migrations/000001_create_users_table.sql
@@ -1,28 +1,52 @@
 -- +goose Up
 
-CREATE TABLE IF NOT EXISTS users (
-                                     id serial primary key,
-                                     login varchar(100) not null,
-                                     uuid uuid not null,
-                                     email varchar(100) not null,
-                                     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+CREATE TABLE IF NOT EXISTS users
+(
+    id               serial primary key,
+    login            varchar(100) not null,
+    uuid             uuid         not null,
+    email            varchar(100) not null,
+    last_avatar_link TEXT         not null,
+    created_at       TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
-CREATE TABLE IF NOT EXISTS data (
-                                    id bigint PRIMARY KEY,
-                                    user_id integer not null,
-                                    name varchar(100),
-                                    surname varchar(100),
-                                    birthdate date,
-                                    telegram varchar(100),
-                                    git varchar(100),
-                                    city integer,
-                                    os integer,
-                                    work integer,
-                                    university integer
+CREATE TABLE IF NOT EXISTS data
+(
+    id            bigint PRIMARY KEY,
+    user_id       integer not null,
+    name          varchar(100),
+    surname       varchar(100),
+    birthdate     date,
+    phone         varchar(15),
+    telegram      varchar(100),
+    git           varchar(100),
+    city_id       integer,
+    os_id         integer,
+    work_id       integer,
+    university_id integer,
+    CONSTRAINT fk_data_user_id FOREIGN KEY (user_id) REFERENCES users(id)
+);
+
+CREATE TABLE IF NOT EXISTS skills
+(
+    id bigint PRIMARY KEY,
+    user_id bigint not null,
+    skill_id bigint not null,
+    CONSTRAINT fk_skill_user_id FOREIGN KEY (user_id) REFERENCES users(id)
+
+);
+
+CREATE TABLE IF NOT EXISTS hobbies
+(
+    id bigint PRIMARY KEY,
+    user_id bigint not null,
+    hobby_id bigint not null,
+    CONSTRAINT fk_hobby_user_id FOREIGN KEY (user_id) REFERENCES users(id)
 );
 
 -- +goose Down
 
 DROP TABLE if exists users;
 DROP TABLE if exists data;
+DROP TABLE if exists skills;
+DROP TABLE if exists hobbies;
